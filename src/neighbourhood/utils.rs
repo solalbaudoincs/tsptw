80767961f6f7e7ba70930dda0cc64b1a8819fed7
solal.baudoin <solal.baudoin@student-cs.fr>
2025-11-22 17:53:53 +0100

    add neighbourhoodmixer

--- src/neighbourhood/utils.rs
+++ src/neighbourhood/utils.rs
@@ -3,12 +3,30 @@ use crate::problem::Solution;
 
 use rand::Rng;
 
-pub struct NeighborMixer {
+pub struct NeighborFnMixer {
     neighborhood_fns: Vec<Box<dyn NeighborFn>>,
     weights: Vec<f64>,
 }
 
-impl NeighborFn for NeighborMixer {
+impl NeighborFnMixer {
+    pub fn new(
+        neighborhood_fns: Vec<Box<dyn NeighborFn>>,
+        weights: Vec<f64>,
+    ) -> Self {
+        let weight_sum: f64 = weights.iter().sum();
+        let normalized_weights: Vec<f64> = weights
+            .iter()
+            .map(|w| *w / weight_sum)
+            .collect();
+
+        NeighborFnMixer {
+            neighborhood_fns,
+            weights: normalized_weights,
+        }
+    }
+}
+
+impl NeighborFn for NeighborFnMixer {
     fn get_neighbor(&self, solution: &Solution) -> Solution {
         let mut rng = rand::rng();
         let r: f64 = rng.random_range(0.0..1.0);
