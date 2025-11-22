9bf957018f4750fc65b08b592cbb81046c5055c7
solal.baudoin <solal.baudoin@student-cs.fr>
2025-11-22 18:42:31 +0100

    refactor TwoOpt to use instance RNG for neighbor generation

--- src/neighbourhood/twoopt.rs
+++ src/neighbourhood/twoopt.rs
@@ -18,16 +18,15 @@ impl NeighborFn for TwoOpt {
     fn get_neighbor(&mut self, solution: &Solution) -> Solution {
         // Implementation of the 2-opt neighbor generation
 
-        let mut rng = rand::rng();
         let len = solution.len();
-        let mut i = rng.random_range(0..len);
+        let mut i = self.rng.random_range(0..len);
         let mut j;
 
         if i == len - 1 {
-            j = rng.random_range(0..len - 1);
+            j = self.rng.random_range(0..len - 1);
             (i, j) = (j, i);
         } else {
-            j = rng.random_range(i + 1..len);
+            j = self.rng.random_range(i + 1..len);
         }
         let mut new_route: Solution = solution.clone();
         new_route[i..=j].reverse();
